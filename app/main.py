236f4d45732ba0824f001d6c70a2b4564507dc19
dishakamore-ship-it <you@example.com>
2026-07-18 14:00:47 +0530

    fix cors

--- app/main.py
+++ app/main.py
@@ -17,8 +17,7 @@ app = FastAPI(title="Personal Finance Tracker API", lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
+        "http://127.0.0.1:5500",
         "http://localhost:5500",
         "https://frontend-ivory-seven-2ahzon8ydl.vercel.app"
     ],
